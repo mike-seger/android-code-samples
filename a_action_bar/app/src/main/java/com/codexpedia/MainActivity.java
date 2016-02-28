ce2f81be2e8ea1b2e18a52b70f2f88bf0d44e8ef
Peng Ye <peng@Pengs-MBP-2.home>
2016-02-28 10:21:50 -0500

    actionbar update

---
+++ a_action_bar/app/src/main/java/com/codexpedia/MainActivity.java
@@ -0,0 +1,67 @@
+package com.codexpedia;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import info.androidhive.actionbar.R;
+
+public class MainActivity extends Activity {
+
+	private ActionBar actionBar;
+	private TextView tvAction;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tvAction = (TextView) findViewById(R.id.tv_action);
+
+		actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false); // Hide the action bar title
+		actionBar.setDisplayHomeAsUpEnabled(true);
+//		actionBar.setHomeAsUpIndicator(R.drawable.ic_action_list);
+
+		actionBar.setIcon(R.drawable.ic_action_list);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.activity_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	/**
+	 * On selecting action bar icons
+	 **/
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				setActionText("actionbar home button clicked!");
+				break;
+			case R.id.btn_edit:
+				setActionText("edit button clicked!");
+				break;
+			case R.id.btn_save:
+				setActionText("save button clicked!");
+				break;
+			case R.id.btn_help:
+				setActionText("help button clicked!");
+				break;
+			default:
+				super.onOptionsItemSelected(item);
+				break;
+		}
+		return true;
+	}
+
+	private void setActionText(String s) {
+		tvAction.setText(s);
+	}
+}
--- a_spinner_drop_down/app/src/main/java/com/codexpedia/spinner/SpinnerActivity.java
+++ a_spinner_drop_down/app/src/main/java/com/codexpedia/spinner/SpinnerActivity.java
@@ -1,4 +1,4 @@
-package com.ui.yogeshblogspot;
+package com.codexpedia.spinner;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -9,19 +9,26 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class SpinnerExActivity extends Activity {
-    /** Called when the activity is first created. */
+import com.ui.yogeshblogspot.R;
+
+public class SpinnerActivity extends Activity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Spinner sp=(Spinner)findViewById(R.id.spinner1);
-        String[] courses={"Android","IPhone","Windows Mobile","Blackberry","J2me"};
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,courses);
-        sp.setAdapter(adapter); 
-        sp.setOnItemSelectedListener(new OnItemSelectedListener() {
+		initSpinner1();
+		initSpinner2();
+    }
+
+	private void initSpinner1() {
+		Spinner sp=(Spinner)findViewById(R.id.spinner1);
+		String[] courses={"Android","IPhone","Windows Mobile","Blackberry","J2me"};
+		ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,courses);
+		sp.setAdapter(adapter);
+		sp.setOnItemSelectedListener(new OnItemSelectedListener() {
 			public void onItemSelected(AdapterView<?> av, View v,
-					int position, long itemId) {
+									   int position, long itemId) {
 				// TODO Auto-generated method stub
 				String item=av.getItemAtPosition(position).toString();
 				Toast.makeText(getApplicationContext(), "Selected Item is "+item, Toast.LENGTH_LONG).show();
@@ -30,14 +37,14 @@ public class SpinnerExActivity extends Activity {
 				// TODO Auto-generated method stub
 			}
 		});
+	}
 
-
-
-        Spinner sp2=(Spinner)findViewById(R.id.spinner2);
-        sp2.setOnItemSelectedListener(new OnItemSelectedListener() {
+	private void initSpinner2() {
+		Spinner sp2=(Spinner)findViewById(R.id.spinner2);
+		sp2.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			public void onItemSelected(AdapterView<?> av, View v,
-					int position, long itemId) {
+									   int position, long itemId) {
 				// TODO Auto-generated method stub
 				String item=av.getItemAtPosition(position).toString();
 				Toast.makeText(getApplicationContext(),"Selected Item is "+item,Toast.LENGTH_LONG).show();
@@ -45,18 +52,15 @@ public class SpinnerExActivity extends Activity {
 
 			public void onNothingSelected(AdapterView<?> av) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-    }
-    public void getDetail(View v)
-    {
+	}
+
+    public void getDetail(View v) {
     	Spinner spinner1=(Spinner)findViewById(R.id.spinner1);
     	Spinner spinner2=(Spinner)findViewById(R.id.spinner2);
     	
     	Toast.makeText(getApplicationContext(), "Spinner 1- "+spinner1.getSelectedItem().toString()+"\nSpinner 2-"+spinner2.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
-    	
     }
-   
-    
 }
